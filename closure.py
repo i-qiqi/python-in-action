71b101c992ec34cd3089b84d609473ff2efcbdf7
biqi zhu <biqi.zhu@sap.com>
2019-11-15 17:34:21 +0800

    closure

--- closure.py
+++ closure.py
@@ -19,3 +19,26 @@ def count_1():
     for i in range(1, 4):
         fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
     return fs
+# 如果在一个内部函数里，对在外部作用域（但不是在全局作用域）的变量进行引用，
+# 那么内部函数就被认为是闭包(closure)
+# addy是一个内部函数，x是外部作用域addx里面，但不在全局作用域里=>addy是一个闭包
+def addx(x):
+    def addy(y): return x + y
+    return addy
+
+# 闭包中不能修改外部作用域的局部变量
+def foo():
+    m = 0
+    def foo1():
+        m = 1
+        print(m)
+    print(m)
+    foo1()
+    print(m)
+
+def foo_err():
+    a = 1
+    def bar():
+        a = a + 1
+        return a
+    return bar
--- closure_test.py
+++ closure_test.py
@@ -1,3 +1,11 @@
-from closure import count,count_1
+from closure import count,count_1,addx,foo, foo_err
 f1, f2, f3= count_1()
 print(f1(), f2(), f3())
+
+c = addx(8)
+print(c.__name__, type(c), c(10))
+
+foo()
+
+c = foo_err()
+print(c())
